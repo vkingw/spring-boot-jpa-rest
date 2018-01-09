91f02b9f22591ed500aa5af9009f56059d3bfed9
vkingw <alfa.king+git@gmail.com>
2018-01-09 17:44:44 +0800

    初始化数据调整

--- src/main/java/com/vincent/example/Application.java
+++ src/main/java/com/vincent/example/Application.java
@@ -1,16 +1,15 @@
 package com.vincent.example;
 
-import com.vincent.example.model.Book;
 import com.vincent.example.model.Company;
 import com.vincent.example.model.Employee;
 import com.vincent.example.model.User;
-import com.vincent.example.repository.BooksRepository;
 import com.vincent.example.repository.CompanyRepository;
 import com.vincent.example.repository.EmployeeRepository;
 import com.vincent.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -26,32 +25,25 @@ import javax.annotation.PostConstruct;
 @EnableWebMvc
 public class Application {
 
-  final
-  CompanyRepository companyRepository;
-  final
-  EmployeeRepository employeeRepository;
-  final
-  BooksRepository booksRepository;
-  final
-  UserRepository userRepository;
+  private final CompanyRepository companyRepository;
+  private final EmployeeRepository employeeRepository;
+  private final UserRepository userRepository;
 
   @Autowired
-  public Application(CompanyRepository companyRepository, EmployeeRepository employeeRepository, BooksRepository booksRepository, UserRepository userRepository) {
+  public Application(CompanyRepository companyRepository, EmployeeRepository employeeRepository, UserRepository
+    userRepository) {
     this.companyRepository = companyRepository;
     this.employeeRepository = employeeRepository;
-    this.booksRepository = booksRepository;
     this.userRepository = userRepository;
   }
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
   public @PostConstruct
   void init() {
 
-    booksRepository.save(new Book("test"));
-    booksRepository.save(new Book("test2"));
     Company apple = companyRepository.save(new Company("Apple"));
     Company google = companyRepository.save(new Company("Google"));
 
@@ -78,8 +70,10 @@ public class Application {
 
     User user = new User();
     user.setName("admin");
-    user.setPassword("password");
+    String stronger_salt = BCrypt.gensalt(8);
+    user.setPassword(BCrypt.hashpw("password", stronger_salt));
     user.setNickname("VK");
+    user.setUserId("admin");
     userRepository.save(user);
 
     User user2 = new User();

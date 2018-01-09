e96f7c38eecb003ed07200cf1ce4ad1ccafb4056
vkingw <alfa.king+git@gmail.com>
2018-01-09 16:43:00 +0800

    对于web socket，token使用url拼接方式

--- src/main/java/com/vincent/example/authorization/filter/ExampleAuthenticationFilter.java
+++ src/main/java/com/vincent/example/authorization/filter/ExampleAuthenticationFilter.java
@@ -1,16 +1,21 @@
 package com.vincent.example.authorization.filter;
 
 import com.vincent.example.config.Constants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static com.vincent.example.config.Constants.SOCKET_PUSH_PATH_NORMAL;
+import static com.vincent.example.config.Constants.SOCKET_PUSH_PATH_ROOT;
 
 /**
  * Created : vincent
  * Date : 2017/8/18 下午5:30
  * Email : wangxiao@wafersystems.com
  */
+@Slf4j
 public class ExampleAuthenticationFilter extends AbstractPreAuthenticatedProcessingFilter {
 
   private static final String SSO_CREDENTIALS = "N/A";
@@ -21,7 +26,26 @@ public class ExampleAuthenticationFilter extends AbstractPreAuthenticatedProcess
 
   @Override
   protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-    return request.getHeader(Constants.AUTHORIZATION);
+    if (request.getServletPath().startsWith(SOCKET_PUSH_PATH_ROOT + SOCKET_PUSH_PATH_NORMAL)) {
+      try {
+        String[] parameterValues = request.getParameterValues(Constants.AUTHORIZATION);
+        if (parameterValues.length > 0) {
+          return parameterValues[0];
+        } else {
+          if (log.isDebugEnabled()) {
+            log.debug("socket not get token", new Object[]{parameterValues});
+          }
+        }
+      } catch (Exception e) {
+        if (log.isDebugEnabled()) {
+          log.debug("socket token error:", (Object[]) request.getParameterValues(Constants.AUTHORIZATION));
+          log.debug("socket token error:", (Object[]) e.getStackTrace());
+        }
+      }
+    } else {
+      return request.getHeader(Constants.AUTHORIZATION);
+    }
+    return null;
   }
 
   @Override

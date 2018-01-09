5f4f999103ae727784006f86e06087b4588f06a4
vkingw <alfa.king+git@gmail.com>
2018-01-09 17:42:52 +0800

    登陆与注销

--- src/main/java/com/vincent/example/controller/TokenController.java
+++ src/main/java/com/vincent/example/controller/TokenController.java
@@ -1,38 +1,44 @@
 package com.vincent.example.controller;
 
-import com.vincent.example.authorization.annotation.Authorization;
 import com.vincent.example.authorization.annotation.CurrentUser;
 import com.vincent.example.authorization.manager.TokenManager;
 import com.vincent.example.authorization.model.TokenModel;
-import com.vincent.example.model.ErrorResultModel;
+import com.vincent.example.exception.ControllerException;
+import com.vincent.example.mapper.LoginModel;
 import com.vincent.example.model.User;
 import com.vincent.example.repository.UserRepository;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Locale;
+
+import static com.vincent.example.config.Constants.AUTHORIZATION;
 
 /**
  * Created : vincent
  * Date : 2017/8/3 下午12:40
  * Email : wangxiao@wafersystems.com
  */
-@RepositoryRestController
-@RequestMapping("/tokens")
+@Controller
+@RequestMapping("${spring.data.rest.base-path}")
+@Api(value = "登录和注销", tags = "登录和注销API", description = "系统登录和注销")
+@Slf4j
 public class TokenController {
-
-  private static final Logger logger = LoggerFactory.getLogger(TokenController.class);
 
   private final MessageSource messageSource;
 
@@ -47,44 +53,42 @@ public class TokenController {
     this.tokenManager = tokenManager;
   }
 
-  @RequestMapping(method = RequestMethod.POST)
-  @ApiOperation(value = "登录")
-  public ResponseEntity<TokenModel> login(@RequestParam String name, @RequestParam String password) {
-    Assert.notNull(name, "name can not be empty");
-    Assert.notNull(password, "password can not be empty");
 
-    User user = userRepository.findUserByName(name);
+  @PostMapping(path = "/login")
+  @ApiOperation(value = "登录")
+  public ResponseEntity<TokenModel> login(@RequestBody @Valid LoginModel loginModel) throws ControllerException {
+    Locale locale = LocaleContextHolder.getLocale();
+
+    User user = userRepository.findUserByUserId(loginModel.getUserId());
     if (user == null ||  //未注册
-      !user.getPassword().equals(password)) {  //密码错误
+      !checkPassword(user, loginModel.getPassword())) {  //密码错误
       //提示用户名或密码错误
-      Locale locale = LocaleContextHolder.getLocale();
-      return new ResponseEntity(ErrorResultModel.error(HttpStatus.NOT_FOUND,
-        messageSource.getMessage("login.error", null, locale)), HttpStatus.NOT_FOUND);
+      throw new ControllerException(HttpStatus.NOT_FOUND, messageSource.getMessage("login.error", null, locale));
     }
     //生成一个token，保存用户登录状态
     TokenModel tokenModel;
     try {
       tokenModel = tokenManager.createToken(user);
     } catch (Exception e) {
-      logger.error("Create Token Error.", e);
-      Locale locale = LocaleContextHolder.getLocale();
-      return new ResponseEntity(ErrorResultModel.error(HttpStatus.EXPECTATION_FAILED,
-        messageSource.getMessage("login.token.create.error", null, locale)), HttpStatus.EXPECTATION_FAILED);
+      log.error("Create Token Error.", e);
+      throw new ControllerException(HttpStatus.EXPECTATION_FAILED, messageSource.getMessage("login.token.create" +
+        ".error", null, locale));
     }
     return ResponseEntity.ok(tokenModel);
   }
 
-  @DeleteMapping
-  @Authorization
+  @DeleteMapping(path = "/logout")
   @ApiOperation(value = "退出登录")
   @ApiImplicitParams({
-    @ApiImplicitParam(name = "authorization", value = "authorization", required = true, dataType =
-      "string", paramType = "header"),
+    @ApiImplicitParam(name = AUTHORIZATION, value = AUTHORIZATION, required = true, dataType =
+      "string", paramType = "header")
   })
-  public @ResponseBody
-  ResponseEntity<?> logout(@CurrentUser User user) {
+  public ResponseEntity logout(@CurrentUser User user) {
     tokenManager.deleteToken(user.getId());
-    return ResponseEntity.ok("");
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
   }
 
+  private boolean checkPassword(User user, String password) {
+    return BCrypt.checkpw(password, user.getPassword());
+  }
 }

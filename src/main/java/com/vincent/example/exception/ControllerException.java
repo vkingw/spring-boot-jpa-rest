a65a86d22a03b73da2fd2ef45bb667a2dbc89f20
vkingw <alfa.king+git@gmail.com>
2018-01-09 17:03:34 +0800

    Controller Exception 统一处理

---
+++ src/main/java/com/vincent/example/exception/ControllerException.java
@@ -0,0 +1,21 @@
+package com.vincent.example.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Created : vincent
+ * Date : 2017/10/9 下午8:27
+ * Email : wangxiao@wafersystems.com
+ */
+@Getter
+public class ControllerException extends Exception {
+
+  private static final long serialVersionUID = -446375198350395568L;
+  private HttpStatus httpStatus;
+
+  public ControllerException(HttpStatus httpStatus, String message) {
+    super(message);
+    this.httpStatus = httpStatus;
+  }
+}
